8b906b08564bf68cf65ffbeae7442a3da0a86755
Danielle Navarro <d.navarro@unsw.edu.au>
2022-01-16 10:03:28 +1100

    continuously varying trajectory lengths

--- R/trajectories.R
+++ R/trajectories.R
@@ -3,11 +3,7 @@ select_trajectory <- function(seed) {
 
   set.seed(seed)
 
-  iterations <- sample(
-    x = c(10^4, 10^5, 10^6),
-    size = 1,
-    prob = c(.2, .2, .6)
-  )
+  iterations <- round(runif(1, min = 10^5, max = 10^6))
 
   dat <- damage_path(iter = iterations)
   colnames(dat) <- c("x0", "y0", "x1", "y1", "shade")
