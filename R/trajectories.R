809f97816c883b9cd9d98d6386c9d6d343ffacab
Danielle Navarro <d.navarro@unsw.edu.au>
2022-01-16 17:44:26 +1100

    longer paths

--- R/trajectories.R
+++ R/trajectories.R
@@ -3,7 +3,7 @@ select_trajectory <- function(seed) {
 
   set.seed(seed)
 
-  iterations <- round(runif(1, min = 10^5, max = 10^6))
+  iterations <- round(runif(1, min = 10^6, max = 10^7))
 
   dat <- damage_path(iter = iterations)
   colnames(dat) <- c("x0", "y0", "x1", "y1", "shade")
